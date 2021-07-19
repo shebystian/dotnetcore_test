a426088e1f92ca9a7a46483bf519e1be0f9147a5
Sebastian Sanchez <ssancheza86@gmail.com>
2021-07-18 23:59:11 -0400

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyMicroservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class TestController: ControllerBase
+    {
+
+        [HttpGet]
+        public string getHello()
+        {
+            return "Hello this is a Test controller.";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotnetcore_test
 first test of microservice in dotnetcore
+
+to run execute:
+
+dotnet run
+open http://localhost:5000
