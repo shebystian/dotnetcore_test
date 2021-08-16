4465e6dd96f1fc3a86f3ff705b73ed634b8eef56
Sebastian Sanchez <sebastian@kranio.io>
2021-08-15 20:52:46 -0400

    add model to bd local in linux

---
+++ Migrations/20210816003930_Game.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyMicroservice.Migrations
+{
+    public partial class Game : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    categoria = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,5 +9,23 @@ open http://localhost:3000
 to create project template
 dotnet new webapi -o MyMicroservice --no-https -f net5.0
 
-for changes in databases
+to compile project:
+dotnet build
+
+to exec:
+dotnet run
+
+for changes in databases (exporta los cambios de la bd a nuestro modelo)
 dotnet ef dbcontext scaffold "Server={INSTANCENAME};Database={DBNAME};Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models
+
+(agregamos una migracion para agregar a la bd se debe especificar el contexto ya que existen mas de 1 por pruebas)
+dotnet ef migrations add Game --context CatalogContext
+
+(agregamos los cambios a la bd desde entity framework)
+dotnet ef database update --context CatalogContext
+
+use sql server in linux:
+sqlcmd -S localhost -U SA -P '<password>'
+
+query example in sqlcmd:
+select * from Catalog.dbo.Game where nombre = 'Doom'
